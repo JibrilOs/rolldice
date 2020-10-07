d8f6611980062a52a742e815e420798f93ef373e
unknown <webxite23@gmail.com>
2020-10-07 06:20:03 +0300

    Updates

---
+++ precache-manifest.7aec2520edcb3c420314d5140d42492f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45d997dfe44b7a091a8e7cf31e33eb85",
+    "url": "/rolldice/index.html"
+  },
+  {
+    "revision": "962b8a446c248e173d66",
+    "url": "/rolldice/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cb4fb5fbd7a1893cabe2",
+    "url": "/rolldice/static/js/2.c16649c8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rolldice/static/js/2.c16649c8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "962b8a446c248e173d66",
+    "url": "/rolldice/static/js/main.1b1d6663.chunk.js"
+  },
+  {
+    "revision": "7dad873734dfead13a5d",
+    "url": "/rolldice/static/js/runtime-main.dd89cbc9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/rolldice/static/media/logo.5d5d9eef.svg"
+  }
+]);
